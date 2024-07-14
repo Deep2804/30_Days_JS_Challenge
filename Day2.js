773e10366599f0ab422fc4c149e7fcea7bcb50e9
Deep2804 <deepak28042002singh@gmail.com>
2024-07-14 23:18:24 +0530

    Operators in JS

---
+++ Day2.js
@@ -0,0 +1,92 @@
+// Activity 1: Arithmetic Operations
+
+// Task 1:Write a program to add two numbers and log the result to the console
+let num1 = 12;
+let num2 = 13;
+let sum = num1+num2;
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
+
+// Task 2:Write a program to subtract two numbers and log the result to the console
+let num_1 = 27;
+let num_2 = 13;
+let sub = num_1-num_2;
+console.log(`The subtract from ${num_1} and ${num_2} is: ${sub}`);
+
+// Task 3:Write a program to multiply two numbers and log the result to the console
+let number1 = 6;
+let number2 = 18;
+let multiply = number1*number2;
+console.log(`The multiply of ${number1} and ${number2} is: ${multiply}`);
+
+// Task 4:Write a program to divide two numbers and log the result to the console
+let number_1 = 108;
+let number_2 = 9;
+let division = number_1/number_2;
+console.log(`The division of ${number_1} and ${number_2} is: ${division}`);
+
+// Task 5:Write a program to find the remainder when one number is divided by another and log the result to the console
+let value1 = 105;
+let value2 = 9;
+let modulus = value1%value2;
+console.log(`The remainder of ${value1} and ${value2} is: ${modulus}`);
+
+
+// Activity 2: Assignment Operators
+
+// Task 6:Use the += operator to add a number to a variable and log the result to the console
+numberA = 19;
+numberA += 11;
+console.log(numberA);
+
+// Task 7:Use the -= operator to subtract a number to a variable and log the result to the console
+number_A = 19;
+number_A -= 11;
+console.log(number_A);
+
+
+
+// Activity 3: Comparisons Operators
+
+// Task 8:Write a program to compare two numbers using > and < and log the result to the console
+x = 15;
+y = 17;
+console.log(x>y);
+console.log(x<y);
+
+// Task 9:Write a program to compare two numbers using >= and <= and log the result to the console
+a = 57;
+b = 43;
+console.log(a <= b);
+console.log(a >= b);
+
+// Task 10:Write a program to compare two numbers using == and === and log the result to the console
+m = 29;
+n = 29;
+console.log(m == n);
+console.log(m === n);
+
+
+
+// Activity 4: Logical Operators
+// Task 11: Write a program that uses && operator to combine two conditions and log the result to the console
+valA = 7;
+valB = 5;
+console.log((valA>valB) && (valA<valB)); 
+
+// Task 12: Write a program that uses || operator to combine two conditions and log the result to the console
+ValA = 7;
+ValB = 5;
+console.log((ValA>ValB) || (ValA<ValB)); 
+
+// Task 13: Write a program that uses ! operator to negate a conditions and log the result to the console
+val_A = true;
+console.log(!val_A); 
+
+
+// Activity 5: Ternary Operator
+
+// Task 14: Write a program that uses ternary operator to check if a number is positive or negative and log the result to the console
+value = 14;
+console.log(value>=0 ? "positive": "negative");
+
+
