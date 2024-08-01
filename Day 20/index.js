d71b702a081a96c04c5fb9c93f26a1ffc53c59f5
Deep2804 <deepak28042002singh@gmail.com>
2024-08-01 23:10:16 +0530

    Local Storage and Session Storage

---
+++ Day 20/index.js
@@ -0,0 +1,20 @@
+// Activity 1: Understanding Local Storage
+// Task 1: Write a script to save a string value to localstorage and retrieve it. Log the desired output.
+// Task 2: Write a script to save an object to localstorage by converting it to json string. Retrieve and parse the object,then log it.
+// Actiity 2: Using localstorage
+// Task 3: Create a simple form that saves user input(e.g., name and email) to localstorage when submitted. Retrieve and displaat the saved data on page load.
+// Task 4: Write a script to remove an item from localstorage. Log the LocalStorage content before and after removal.
+// Activity 3: Understanding Session Storage
+// Task 5: Write a script to save a string value to session storage and retrieve it. Log the retrieved value.
+// Task 6: Write a script to save an object to session storage by converting it to the JSON string. Retrieve and parse the object, then log it. 
+// Activity 4: Using Session Storage
+// Task 7: Create a simple form that saves user input(e.g., name and email) to sessionstorage when submitted. Retrieve and displaat the saved data on page load. 
+// Task 8: Write a script to remove an item from sessionstorage. Log the SessionStorage content before and after removal.
+// Activity 5: Comparing LocalStorage and SessionStorage
+// Task 9: Write a function that accepts a key and a value, and saves the values to both localstorage and sessionstorage. Retrieve and log the values from both storage mechanism.
+// Task 10: Write a function that clears all data from both localstorage and sessionstorage. Verify that both storages are empty.
+// 
+// 
+// 
+// 
+// 
